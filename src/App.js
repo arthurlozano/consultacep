b298183eb8a55348061e0c21311cc43aba43d729
Arthur Lozano <athlonlozano@gmail.com>
2022-03-22 15:30:55 -0300

    Commit da aplicação pronta de busca de CEP

--- src/App.js
+++ src/App.js
@@ -1,27 +1,75 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 import './styles.css';
 
 function App() {
+
+  const [cep, setCep] = useState('');
+  const [dadosCep, setDadosCep] = useState({});
+
+  async function handleSearch() {
+    if (cep === '') {
+      alert("Preencha algum CEP");
+      return;
+    }
+
+    try {
+      const res = await api.get(`${cep}/json`);
+      setDadosCep(res.data);
+      setCep('');
+    } catch (err) {
+      alert("Algum erro ocorreu");
+      console.log({
+        msg: "Ocorreu um erro ao buscar o CEP",
+        err: err
+      });
+      setCep("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite o CEP" />
+        <input
+          type="text"
+          placeholder="Digite o CEP"
+          value={cep}
+          onChange={(e) => setCep(e.target.value)}
+        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
+      {Object.keys(dadosCep).length > 0 && (
+        <main className="main">
+          {dadosCep.logradouro === "" ? (
+            <>
+              <h2>CEP: {dadosCep.cep}</h2>
 
-      <main className="main">
-        <h2>CEP: 15910000</h2>
+              <span>CEP sem informações de logradouro</span>
+              <span>{dadosCep.localidade} - {dadosCep.uf}</span>
+              <span>DDD: {dadosCep.ddd}</span>
+              <span>Código IBGE: {dadosCep.ibge}</span>
+            </>
+          ) : (
+            <>
+              <h2>CEP: {dadosCep.cep}</h2>
 
-        <span>Rua Teste Algum</span>
-        <span>Complemento: algum complemento</span>
-        <span>Bairro: Vila Rosa</span>
-        <span>Campo grande - MS</span>
-      </main>
+              <span>{dadosCep.logradouro}</span>
+              {dadosCep.complemento !== "" && <span>Complemento: {dadosCep.complemento}</span>}
+              <span>Bairro: {dadosCep.bairro}</span>
+              <span>{dadosCep.localidade} - {dadosCep.uf}</span>
+              <span>DDD: {dadosCep.ddd}</span>
+              <span>Código IBGE: {dadosCep.ibge}</span>
+            </>
+          )}
+        </main>
+      )}
+
     </div>
   );
 }
